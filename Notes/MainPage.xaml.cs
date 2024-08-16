1e0e6150a641cc11dfbcf6d497808e5f0a31a2c4
Felipe-fbastos <felipeferreirabastos2@gmail.com>
2024-08-16 19:43:45 -0300

    Subindo para o git

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+
+        string filepath = Path.Combine(FileSystem.AppDataDirectory, "Notes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(filepath))
+            {
+                EditorSalvar.Text = File.ReadAllText(filepath);
+            }
+
+        }
+
+        public void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            String texto = EditorSalvar.Text;
+
+            File.WriteAllText(filepath,texto);
+            DisplayAlert("Salvo", "Seu Arquivo foi salvo", "ok");
+            
+
+
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+
+            if (File.Exists(filepath))
+            {
+                File.Delete(filepath);
+                DisplayAlert("Operação concluida", "Arquivo deletado com Sucesso", "Ok");
+                EditorSalvar.Text = null;
+            }
+            else
+            {
+                DisplayAlert("Alerta", "O Arquivo já foi deletado", "Ok");
+            }
+        }
+    }
+
+}
